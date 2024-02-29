7e34381e14a73b8767dc5174c0ea20f5a5c1e6e8
AndreaSciolla <andrea.sciolla@itiscuneo.eu>
2024-02-29 12:54:06 +0100

    Upload Assets

---
+++ Assets/Scripts/ControlCambia.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.IO;
+
+/**
+ * 
+ */
+public class GestoreBottoni : MonoBehaviour
+{
+    public string percorsoFileJSON = "questions/history/"; // Percorso predefinito del file JSON
+
+    public void CambiaScenaEMemorizzaPercorso()
+    {
+        string livello = PlayerPrefs.GetString("LivelloJSON", "");
+        string percorsoCompleto = percorsoFileJSON + livello;
+        PlayerPrefs.SetString("PercorsoFileJSON", percorsoCompleto);
+
+        SceneManager.LoadScene(1);
+    }
+}
